3fe0313a5777ea14b0496176aab25a6e979b6c5d
tfermoso <tomas.fermoso@gmail.com>
2025-12-09 18:01:14 +0100

    creado formulario para nuevo profesor

--- nuevo_profesor.php
+++ nuevo_profesor.php
@@ -20,6 +20,43 @@ if($consultasResult->num_rows>0){
         $consultas[]=$fila;
     }
 }
-var_dump($consultas)
 
 ?>
+<!DOCTYPE html>
+<html lang="es">        
+<head>
+    <meta charset="UTF-8">
+    <title>Nuevo Profesor</title>
+</head>
+<body>
+<h1>Agregar Nuevo Profesor</h1>
+<form method="POST" action="procesar_nuevo_profesor.php">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" required><br><br>
+
+    <label for="despacho">Despacho:</label>
+    <input type="text" id="despacho" name="despacho" required><br><br>
+
+    <label for="area_id">Área:</label>
+    <select id="area_id" name="area_id" required>
+        <option value="">-- Selecciona un área --</option>
+        <?php foreach($areas as $area): ?>
+            <option value="<?php echo $area['area_id']; ?>"><?php echo $area['nombre']; ?></option>
+        <?php endforeach; ?>
+    </select><br><br>
+
+    <label for="consultas_id">Consultas:</label>
+    <select id="consultas_id" name="consultas_id" required>
+        <option value="">-- Selecciona una consulta --</option>
+        <?php foreach($consultas as $consulta): ?>
+            <option value="<?php echo $consulta['consultas_id']; ?>">
+                <?php echo $consulta['dia'] . " a las " . $consulta['hora']; ?>
+            </option>
+        <?php endforeach; ?>
+    </select><br><br>
+
+    <input type="submit" value="Agregar Profesor">
+
+</form>
+</body>
+</html>
