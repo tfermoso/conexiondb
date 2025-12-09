5aefb6859de680f01619cd8ab598df439092f6d5
tfermoso <tomas.fermoso@gmail.com>
2025-12-09 17:53:19 +0100

    leo areas

--- nuevo_profesor.php
+++ nuevo_profesor.php
@@ -3,5 +3,15 @@ require_once "config.php";
 require_once "./includes/functions.php";
 
 $conn=conectarBaseDatos();
+$queryAreas="SELECT area_id, nombre FROM area ORDER BY nombre";
+$areasResult=$conn->query($queryAreas);
+$areas=[];
+if($areasResult->num_rows>0){
+    while($fila=$areasResult->fetch_assoc()){
+        $areas[]=$fila;
+    }
+}
+
+var_dump($areas);
 
 ?>
